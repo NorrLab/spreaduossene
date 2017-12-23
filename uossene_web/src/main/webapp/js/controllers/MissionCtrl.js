482d1db9afb5a87d7fbfde9f690c17745beb8414
ulrich ossene <ulrich.ossene@gmail.com>
2017-12-23 16:53:25 +0100

    #0006 little css anmation

--- uossene_web/src/main/webapp/js/controllers/MissionCtrl.js
+++ uossene_web/src/main/webapp/js/controllers/MissionCtrl.js
@@ -1,6 +1,15 @@
 app.controller("MissionCtrl",["$scope","$location","$timeout","$mdDialog","$translate", function($scope,message,$location,$timeout,
 		$mdDialog,$translate) {
 	
-	
+	$scope.getPhoneNumber = function($scope,$timeout) {
+
+		var myEl = angular.element( document.querySelector( '#my_phone_class' ) );
+
+		if(myEl.hasClass('layout-align-center-stretch')){
+			 myEl.removeClass('layout-align-center-stretch');
+		}else{
+			 myEl.addClass('layout-align-center-stretch');
+		}
+	}
 	
 }])
